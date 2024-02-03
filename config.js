0d089c68a78527141f7ead7b1d8f4fd4d8508d84
Zdenek Slezak <Zdenek.Slezak@myq-solution.com>
2024-02-03 16:32:58 +0100

    Multithread update

--- config.js
+++ config.js
@@ -46,23 +46,27 @@ let config = {
         {
             module: "MMM-PID",
             position: "top_left",
-            accessToken: "your_token_here",
             config: {
                 feeds: [
                     {
                         aswIds: "522",
+                        accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjQxMywiaWF0IjoxNzA2NjI3MTU5LCJleHAiOjExNzA2NjI3MTU5LCJpc3MiOiJnb2xlbWlvIiwianRpIjoiMmViMzMxMGMtYWM5ZS00OGNmLTkwOTktYWFlZGMyOTIwNzdlIn0.7dxNbLlK-DbF-L4WW30cgOHihU4h7kgIXe_hHD8wha4",
+        
                     },
                     {
                         ids: "U522Z1P",
-                        limit: 10
+                        limit: 2
+                        accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjQxMywiaWF0IjoxNzA2NjI3MTU5LCJleHAiOjExNzA2NjI3MTU5LCJpc3MiOiJnb2xlbWlvIiwianRpIjoiMmViMzMxMGMtYWM5ZS00OGNmLTkwOTktYWFlZGMyOTIwNzdlIn0.7dxNbLlK-DbF-L4WW30cgOHihU4h7kgIXe_hHD8wha4",
+        
                     },
                     {
                         ids: "U522Z2P",
                         title: "Title"
+                        accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjQxMywiaWF0IjoxNzA2NjI3MTU5LCJleHAiOjExNzA2NjI3MTU5LCJpc3MiOiJnb2xlbWlvIiwianRpIjoiMmViMzMxMGMtYWM5ZS00OGNmLTkwOTktYWFlZGMyOTIwNzdlIn0.7dxNbLlK-DbF-L4WW30cgOHihU4h7kgIXe_hHD8wha4",
                     }
                 ],
             }
-        },        
+        },     
         {
             module: "weather",
             position: "top_right",
